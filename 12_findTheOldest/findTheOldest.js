76581b1f1cae4431e4ea7187b9ca2fa7b576a15d
jackiewaang <61704082+jackiewaang@users.noreply.github.com>
2024-08-10 13:03:55 +0200

    completed exercises

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((oldest, person) => getAge(person) > getAge(oldest) ? person:oldest, people[0]);
 };
 
+function getAge(person){
+    if(person.yearOfDeath == undefined){
+        return 2024 - person.yearOfBirth;
+    }
+    return person.yearOfDeath - person.yearOfBirth;
+}
 // Do not edit below this line
 module.exports = findTheOldest;
